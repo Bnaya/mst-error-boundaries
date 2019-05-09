0ca934c4832651bc75bdee8f3906fb922906f531
Bnaya Peretz <bnaya@brandtotal.com>
2019-05-09 21:30:45 +0300

    addMSTErrorBoundary intro

--- src/mst.test.ts
+++ src/mst.test.ts
@@ -1,9 +1,14 @@
 import { getSnapshot } from "mobx-state-tree";
 import { Point, Box, Scene } from "./mst";
 
+// jest > 24.5 has annoying issue with inline snapshot formatting right now
+// https://github.com/facebook/jest/issues/8424
+
 describe("mst error boundaries test", () => {
-  test("basic point falls back to 0 0 ", () => {
-    const point = Point.create({} as any);
+  test("When creating a point from a bad snapshot, fallback to 0 0 ", () => {
+    const badPointSnapshot: typeof Point.CreationType = {} as any;
+
+    const point = Point.create(badPointSnapshot);
 
     expect(getSnapshot(point)).toMatchInlineSnapshot(`
       Object {
@@ -13,7 +18,37 @@ describe("mst error boundaries test", () => {
     `);
   });
 
-  test("basic box falls back to ??", () => {
+  test(
+    "When creating box with bad point snapshot inside," +
+      " the point will fallback to 0 0 as above," +
+      "but the good values of the box will be used",
+    () => {
+      const box = Box.create({
+        height: 333,
+        origin: "bottom-left",
+        width: 333,
+        point: {
+          // ops string and not a number
+          x: "5",
+          y: "5"
+        } as any
+      });
+
+      expect(getSnapshot(box)).toMatchInlineSnapshot(`
+        Object {
+          "height": 333,
+          "origin": "bottom-left",
+          "point": Object {
+            "x": 0,
+            "y": 0,
+          },
+          "width": 333,
+        }
+      `);
+    }
+  );
+
+  test("Now we create a bad box, with also bad point inside", () => {
     const box = Box.create({
       height: "333" as any,
       origin: "bottom-left",
@@ -25,65 +60,71 @@ describe("mst error boundaries test", () => {
     });
 
     expect(getSnapshot(box)).toMatchInlineSnapshot(`
-            Object {
-              "height": 100,
-              "origin": "center",
-              "point": Object {
-                "x": 1,
-                "y": 1,
-              },
-              "width": 100,
-            }
-        `);
-  });
-
-  test("basic Scene falls back to ??", () => {
-    const scene = Scene.create({
-      boxes: [
-        {
-          height: 50,
-          origin: "bottom-left",
-          width: 45,
-          point: {
-            x: "5",
-            y: "5"
-          } as any
+      Object {
+        "height": 100,
+        "origin": "center",
+        "point": Object {
+          "x": 1,
+          "y": 1,
         },
-        {
-          height: 50,
-          origin: "bottom-left",
-          width: 45,
-          point: {
-            x: "5",
-            y: "5"
-          } as any
-        }
-      ]
-    });
-
-    expect(getSnapshot(scene)).toMatchInlineSnapshot(`
-                              Object {
-                                "boxes": Array [
-                                  Object {
-                                    "height": 50,
-                                    "origin": "bottom-left",
-                                    "point": Object {
-                                      "x": 0,
-                                      "y": 0,
-                                    },
-                                    "width": 45,
-                                  },
-                                  Object {
-                                    "height": 50,
-                                    "origin": "bottom-left",
-                                    "point": Object {
-                                      "x": 0,
-                                      "y": 0,
-                                    },
-                                    "width": 45,
-                                  },
-                                ],
-                              }
-                    `);
+        "width": 100,
+      }
+    `);
   });
+
+  test(
+    "Here we see a Scene, with one bad nested point inside," +
+      " and the error boundary of the point selectively fix it" +
+      "so we don't need to supply full fallback",
+    () => {
+      const scene = Scene.create({
+        boxes: [
+          {
+            height: 50,
+            origin: "bottom-left",
+            width: 45,
+            // im a bad point
+            point: {
+              x: "5",
+              y: "5"
+            } as any
+          },
+          {
+            height: 66,
+            origin: "bottom-left",
+            width: 88,
+            point: {
+              x: 5,
+              y: 5
+            }
+          }
+        ]
+      });
+
+      expect(getSnapshot(scene)).toMatchInlineSnapshot(`
+        Object {
+          "boxes": Array [
+            Object {
+              "height": 50,
+              "origin": "bottom-left",
+              "point": Object {
+                "x": 0,
+                "y": 0,
+              },
+              "width": 45,
+            },
+            Object {
+              "height": 66,
+              "origin": "bottom-left",
+              "point": Object {
+                "x": 5,
+                "y": 5,
+              },
+              "width": 88,
+            },
+          ],
+        }
+      `);
+    }
+  );
 });
--- src/mst.ts
+++ src/mst.ts
@@ -1,67 +1,75 @@
-import { types, typecheck } from "mobx-state-tree";
+import { types, typecheck, IAnyModelType } from "mobx-state-tree";
 
-function makeTuple<TARGS extends (string | number)[]>(...args: TARGS) {
-    return args;
+export function addMSTErrorBoundary<IT extends IAnyModelType>(
+  Model: IT,
+  /**
+   * Supply a valid snapshot to use, instead of the failing one
+   * We also give you the validation error of the snapshot, and the bad snapshot.
+   */
+  getFallbackSnapshot: (
+    error: unknown,
+    originalButBadSnapshot: unknown
+  ) => IT["CreationType"]
+) {
+  return types.snapshotProcessor(Model, {
+    preProcessor(snMaybeBad: IT["CreationType"]): IT["CreationType"] {
+      try {
+        typecheck(Model, snMaybeBad);
+        return snMaybeBad;
+      } catch (e) {
+        return getFallbackSnapshot(e, snMaybeBad);
+      }
+    }
+  });
 }
 
-export const PointBase = types.model("Point", {
+function makeTuple<TARGS extends (string | number)[]>(...args: TARGS) {
+  return args;
+}
+
+export const Point = addMSTErrorBoundary(
+  types.model("Point", {
     x: types.number,
     y: types.number
-});
+  }),
+  (_error, _originalBadSnapshot) => {
+    //console.warn(_error)
+    return {
+      x: 0,
+      y: 0
+    };
+  }
+);
 
-export const Point = types.snapshotProcessor(PointBase, {
-    preProcessor(sn: typeof PointBase.SnapshotType): typeof PointBase.SnapshotType {
-        try {
-            typecheck(PointBase, sn)
-            return sn;
-        } catch (e) {
-            // console.error(e);
-            return {
-                x: 0,
-                y: 0
-            }
-        }
-    }
-});
-
-export const BoxBase = types.model("Box", {
+export const Box = addMSTErrorBoundary(
+  types.model("Box", {
     point: Point,
     width: types.number,
     height: types.number,
-    origin: types.enumeration("Origin", makeTuple("top-left", "top-right", "center", "bottom-left", "bottom-right")), 
-});
+    origin: types.enumeration(
+      "Origin",
+      makeTuple(
+        "top-left",
+        "top-right",
+        "center",
+        "bottom-left",
+        "bottom-right"
+      )
+    )
+  }),
+  () => ({
+    point: { x: 1, y: 1 } as any,
+    width: 100,
+    height: 100,
+    origin: "center" as const
+  })
+);
 
-export const Box = types.snapshotProcessor(BoxBase, {
-    preProcessor(sn: typeof BoxBase.SnapshotType): typeof BoxBase.SnapshotType {
-        try {
-            typecheck(BoxBase, sn)
-            return sn;
-        } catch (e) {
-            // console.error(e);
-            return {
-                point: {x: 1, y: 1} as any,
-                width: 100,
-                height: 100,
-                origin: "center"
-            }
-        }
-    }
-});
-
-export const SceneBase = types.model("Scene", {
+export const Scene = addMSTErrorBoundary(
+  types.model("Scene", {
     boxes: types.array(Box)
-});
-
-export const Scene = types.snapshotProcessor(SceneBase, {
-    preProcessor(sn: typeof SceneBase.SnapshotType): typeof SceneBase.SnapshotType {
-        try {
-            typecheck(SceneBase, sn)
-            return sn;
-        } catch (e) {
-            // console.error(e);
-            return {
-                boxes: []
-            }
-        }
-    }
-});
+  }),
+  () => ({
+    boxes: []
+  })
+);
